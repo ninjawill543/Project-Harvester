0468152f82e3d3466ca9cf10b6bbd873546a859e
ninjawill543 <lucas.hanson@ynov.com>
2024-03-17 15:27:42 +0100

    working on using youtube api to get thumbnails

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+API_KEY=$(cat key.txt)
+
+video_save=""
+while true
+do
+    curl_output=$(curl -G -s "https://www.googleapis.com/youtube/v3/search" \
+        -d channelId="UCLVMKLtleeiKs0Oqp86ApTQ" \
+        -d key="$API_KEY" \
+        -d part="snippet" \
+        -d order="date")
+    newest_video_url=$(echo "$curl_output" | jq -r '.items | sort_by(.snippet.publishedAt) | last | .snippet.thumbnails.high.url')
+    if ($newest_video_url == $video_save)
+    then
+        echo "Newest video already seen"
+    else
+        echo "New video thumbnai: $newest_video_url"
+    fi
+    #$newest_video_url
+    sleep 10
+done
